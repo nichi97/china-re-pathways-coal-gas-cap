73a25d55440fea10ae0c733b4ca33eb7b366f54f
nichi97 <chigao97@gmail.com>
2022-04-07 16:02:37 -0700

    debuging trouble makers

--- script/vintage_analysis.R
+++ script/vintage_analysis.R
@@ -15,13 +15,18 @@ df_coal_retire_trend <-
 # mean lifespan: 22 years
 avg_lifetime <- mean(df_coal_retire_trend$lifeSpan, na.rm=T) %>% floor()
 
-# lifespan vs starting year
+# In retired plants, lifespan vs starting year
 ggplot(df_coal_retire_trend, aes(y = lifeSpan, x = Year)) + 
   geom_point(position = "jitter") + 
   geom_smooth() + 
   labs(title = "Lifespan of Chinese Coal Plants over Time", 
        x = "Operation start year", 
        y = "Lifespan")
+
+# linear model between lifespan and year
+retire_model <- lm(lifeSpan ~ Year, df_coal_retire_trend)
+
+
 
 # lifespan vs year of retirement: very little predictive power.
 ggplot(df_coal_retire_trend, aes(y = lifeSpan, x = RETIRED)) + 
@@ -62,8 +67,8 @@ ggplot(aes(x = `Planned Retire`)) +
 # ------------------------- Coal ----------------------------------------------------
 
 # 1. Filter out cancelled, shelved, pre-permit, mothballed, and announced coal plants 
-coal_vintage <- coal %>% filter(!Status %in% c("cancelled", "shelved", 
-                                               "pre-permit", "mothballed", "announced"))
+coal_vintage <- coal %>% 
+  filter(!Status %in% c("cancelled", "shelved",  "pre-permit", "mothballed", "announced"))
 
 # Initialize list of years that acts like a dictionary. Store (year, change in capacity) pair
 list_name = (coal_vintage$Year %>% min(na.rm=T)):2080 %>% as.character()
@@ -72,6 +77,10 @@ names(year_ls) <- list_name
 for (i in seq_along(year_ls)){
   year_ls[[i]] <- 0
 }
+
+# change this to set default planned lifetime and construction for coal plants
+PLANNED_LIFETIME <- 30
+CONSTRUCTION_TIME <- 5 
 
 # run the main loop that calculate change in capacity for each year
 for(i in 1:nrow(coal_vintage)){
@@ -96,7 +105,48 @@ for(i in 1:nrow(coal_vintage)){
     calc_start_yr <- curr_retire_yr - avg_lifetime
     year_ls[[as.character(calc_start_yr)]] <- year_ls[[as.character(calc_start_yr)]] + curr_cap
     year_ls[[as.character(curr_retire_yr)]] <- year_ls[[as.character(curr_retire_yr)]] - curr_cap
-  }
+    
+  # 3. retired plant with start year but not retirement year 
+  # Use start year to predict lifetime
+  # We can do this because strong relationship between lifetime and start year (r^2 > 0.8) 
+  } else if (curr_status == "retired" & !is.na(curr_start_yr) & is.na(curr_retire_yr)){
+    curr_frame <- data.frame(Year = curr_start_yr)
+    calc_retire_yr <- predict(retire_model, curr_frame) %>% round() + curr_start_yr 
+    calc_retire_yr <- calc_retire_yr %>% as.character()
+    year_ls[[as.character(curr_start_yr)]] <- year_ls[[as.character(curr_start_yr)]] + curr_cap
+    year_ls[[as.character(calc_retire_yr)]] <- year_ls[[as.character(calc_retire_yr)]] - curr_cap
+    
+  # 4. retired plant without start year nor retirement year
+  } else if (curr_status == "retired" & is.na(curr_start_yr) & is.na(curr_retire_yr)){
+    next
+    
+  # 5. Operating plants with start year and planned retirement year and planned retirement year > 2022
+  # calculate normally
+  } else if (curr_status == "operating" & !is.na(curr_start_yr) & 
+             !is.na(curr_plan_retire_yr & curr_plan_retire_yr > 2022)){
+    year_ls[[as.character(curr_start_yr)]] <-
+      year_ls[[as.character(curr_start_yr)]] + curr_cap
+    year_ls[[as.character(curr_plan_retire_yr)]] <- 
+      year_ls[[as.character(curr_plan_retire_yr)]] - curr_cap
+    
+  # 6. operating plants with start year, planned retirement year, but planned retirement year < 2022
+  # 7. Operating plants with start year but no planned retirement year 
+  # GEM need to update their data 
+  # disregard planned retirement year, use planned_lifetime to calculate retirement year
+  } else if ((curr_status == "operating" & !is.na(curr_start_yr) & 
+             !is.na(curr_plan_retire_yr & curr_plan_retire_yr <= 2022)) | 
+             (curr_status == "operating" & !is.na(curr_start_yr) & 
+              is.na(curr_plan_retire_yr))){
+    
+    calc_planned_retire_yr <- curr_start_yr + planned_lifetime
+    
+    year_ls[[as.character(curr_start_yr)]] <-
+      year_ls[[as.character(curr_start_yr)]] + curr_cap
+    year_ls[[as.character(calc_planned_retire_yr)]] <- 
+      year_ls[[as.character(calc_planned_retire_yr)]] - curr_cap
+    
+  # 8. under construction
+  } else if (curr_status == "construction")
 }
 
 # 2.1  
